23a7b409c549d70f7580da4d02234772dd073393
EunoiaCody <noemail@example.com>
2025-11-29 13:35:13 +0800

    更新配置文件 - 2025-11-29 13:35:13
    自动同步 ~/.config/ 中的配置文件到 dotfiles 仓库

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -4,7 +4,11 @@ return {
   },
   {
     "olimorris/codecompanion.nvim",
-    opts = {},
+    version = "v17.33.0",
+    opts = {
+      language = "Chinese",
+      send_code = true,
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
