17593c2468c11e46e3e42a24b5f349fe307cfa30
EunoiaCody <noemail@example.com>
2025-11-29 10:16:57 +0800

    更新配置文件 - 2025-11-29 10:16:57
    自动同步 ~/.config/ 中的配置文件到 dotfiles 仓库

--- nvim/lua/plugins/code-runner.lua
+++ nvim/lua/plugins/code-runner.lua
@@ -1,5 +1,29 @@
 return {
-  'stevearc/overseer.nvim',
-  opts = {},
-
+  {
+    "CRAG666/code_runner.nvim",
+    config = function ()
+      require("code_runner").setup({
+        mode = "toggleterm",
+        focus = true,
+        startinsert = true,
+        term = {
+          position = "bottom",
+          size = 8,
+        },
+        filetype = {
+          python = "python3 -u",
+          javascript = "node",
+          typescript = "deno run",
+          rust = "cargo run",
+          go = "go run",
+          java = "javac $fileName && java $fileNameWithoutExt",
+          c = "gcc $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+          cpp = "g++ $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+          lua = "lua",
+          sh = "bash",
+        }
+      }
+    )
+    end
+  }
 }
