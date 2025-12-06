e8cedd295b8dc64dfa9435878eff0b5f90bfd1ab
EunoiaCody <noemail@example.com>
2025-12-06 17:12:03 +0800

    更新配置文件 - 2025-12-06 17:12:03
    自动同步 ~/.config/ 中的配置文件到 dotfiles 仓库

--- restore-config.sh
+++ restore-config.sh
@@ -13,7 +13,7 @@ echo "开始还原配置文件到 ~/.config/ ..."
 
 cd "$(dirname "$0")"
 
-CONFIG_DIRS=("kitty" "mpv" "neovide" "nvim", "fish")
+CONFIG_DIRS=("kitty" "mpv" "neovide" "nvim" "fish")
 
 for dir in "${CONFIG_DIRS[@]}"; do
     SRC="$dir"
