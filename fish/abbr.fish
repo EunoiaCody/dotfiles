9279c305e2660f8f8b84c4505dbbae15661bdedd
EunoiaCody <noemail@example.com>
2025-12-07 09:29:56 +0800

    更新配置文件 - 2025-12-07 09:29:55
    自动同步 ~/.config/ 中的配置文件到 dotfiles 仓库

---
+++ fish/abbr.fish
@@ -0,0 +1,22 @@
+# General abbr
+abbr --add nvi neovide
+
+# dnf abbr
+abbr --add dnfu "sudo dnf update; sudo dnf upgrade; sudo dnf autoremove "
+abbr --add dnfi "sudo dnf install"
+abbr --add dnfr "sudo dnf remove"
+abbr --add dnfs "dnf search"
+abbr --add dnfl "dnf list --installed"
+abbr --add dnfc "sudo dnf clean all"
+
+# pacman abbr
+abbr --add pacu "sudo pacman -Syu"
+abbr --add paci "sudo pacman -S"
+abbr --add pacr "sudo pacman -R"
+abbr --add pacs "pacman -Ss"
+
+# git abbr
+abbr --add gits "git status"
+abbr --add gitc "git commit -m"
+abbr --add gitp "git push"
+abbr --add gitpl "git pull"
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,41 +1,41 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        }
-      }
-    }
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-        ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "pyright" },
-    },
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
-  },
-  {
-    'stevearc/conform.nvim',
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "black", "isort" },
-        rust = { "rustfmt" },
-        cpp = { "clang_format" },
-        typescript = { "prettierd" },
-        javascript = { "prettierd" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      }
-    },
-  },
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "rust_analyzer", "pyright" },
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black", "isort" },
+				rust = { "rustfmt" },
+				cpp = { "clang_format" },
+				typescript = { "prettierd" },
+				javascript = { "prettierd" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		},
+	},
 }
