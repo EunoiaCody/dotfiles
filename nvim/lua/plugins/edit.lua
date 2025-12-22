f1852e0dff4492e45076878f66031350cb8f23ad
EunoiaCody <eunoiacody@gmail.com>
2025-12-22 15:18:01 +0800

    更新配置文件 - 2025-12-22 15:18:01
    自动同步 ~/.config/ 中的配置文件到 dotfiles 仓库

--- nvim/lua/plugins/edit.lua
+++ nvim/lua/plugins/edit.lua
@@ -1,26 +1,96 @@
 return {
 
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true,
-    opts = {},
-  },
-  {
-    "kylechui/nvim-surround",
-    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end
-  },
-  {
-    "catgoose/nvim-colorizer.lua",
-    event = "BufReadPre",
-    opts = { -- set to setup table
-    },
-  }
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		opts = {},
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	{
+		"catgoose/nvim-colorizer.lua",
+		event = "BufReadPre",
+		opts = { -- set to setup table
+		},
+	},
+	{
+		"jake-stewart/multicursor.nvim",
+		branch = "1.0",
+		config = function()
+			local mc = require("multicursor-nvim")
+			mc.setup()
 
+			local set = vim.keymap.set
+
+			-- 在主光标上方/下方添加或跳过光标
+			set({ "n", "x" }, "<up>", function()
+				mc.lineAddCursor(-1)
+			end)
+			set({ "n", "x" }, "<down>", function()
+				mc.lineAddCursor(1)
+			end)
+			set({ "n", "x" }, "<leader><up>", function()
+				mc.lineSkipCursor(-1)
+			end)
+			set({ "n", "x" }, "<leader><down>", function()
+				mc.lineSkipCursor(1)
+			end)
+
+			-- 通过匹配单词/选区来添加或跳过光标
+			set({ "n", "x" }, "<leader>n", function()
+				mc.matchAddCursor(1)
+			end)
+			set({ "n", "x" }, "<leader>s", function()
+				mc.matchSkipCursor(1)
+			end)
+			set({ "n", "x" }, "<leader>N", function()
+				mc.matchAddCursor(-1)
+			end)
+			set({ "n", "x" }, "<leader>S", function()
+				mc.matchSkipCursor(-1)
+			end)
+
+			-- 使用 Ctrl + 左键点击添加/移除光标
+			set("n", "<c-leftmouse>", mc.handleMouse)
+			set("n", "<c-leftdrag>", mc.handleMouseDrag)
+			set("n", "<c-leftrelease>", mc.handleMouseRelease)
+
+			-- 禁用和启用光标
+			set({ "n", "x" }, "<c-q>", mc.toggleCursor)
+
+			-- 多光标模式下的特殊键映射层
+			mc.addKeymapLayer(function(layerSet)
+				-- 选择不同的光标作为主光标
+				layerSet({ "n", "x" }, "<left>", mc.prevCursor)
+				layerSet({ "n", "x" }, "<right>", mc.nextCursor)
+
+				-- 删除主光标
+				layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+
+				-- 使用 Esc 启用和清除光标
+				layerSet("n", "<esc>", function()
+					if not mc.cursorsEnabled() then
+						mc.enableCursors()
+					else
+						mc.clearCursors()
+					end
+				end)
+			end)
+
+			-- 自定义光标外观
+			local hl = vim.api.nvim_set_hl
+			hl(0, "MultiCursorCursor", { reverse = true })
+			hl(0, "MultiCursorVisual", { link = "Visual" })
+			hl(0, "MultiCursorSign", { link = "SignColumn" })
+		end,
+	},
 }
