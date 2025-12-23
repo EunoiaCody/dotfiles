564a114eb14a9ef2440a949a969a2bc2075abd27
EunoiaCody <eunoiacody@gmail.com>
2025-12-23 11:51:15 +0800

    更新配置文件 - 2025-12-23 11:51:15
    自动同步 ~/.config/ 中的配置文件到 dotfiles 仓库

--- nvim/lua/plugins/code-runner.lua
+++ nvim/lua/plugins/code-runner.lua
@@ -1,30 +1,29 @@
 return {
-  {
-    "CRAG666/code_runner.nvim",
-    config = function()
-      require("code_runner").setup({
-        mode = "toggleterm",
-        focus = true,
-        startinsert = true,
-        term = {
-          position = "bottom",
-          size = 8,
-        },
-        filetype = {
-          python = "cd $dir && python3 -u",
-          javascript = "cd $dir && node",
-          typescript = "cd $dir && tsc $fileName && node $fileNameWithoutExt.js",
-          rust = "cd $dir && cargo run",
-          go = "cd $dir && go run",
-          java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
-          c = "cd $dir && gcc $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
-          cpp = "cd $dir && g++ $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
-          lua = "cd $dir && lua",
-          sh = "cd $dir && bash",
-          vue = "cd $dir && npm run dev $end"
-        }
-      }
-      )
-    end
-  }
+	{
+		"CRAG666/code_runner.nvim",
+		config = function()
+			require("code_runner").setup({
+				mode = "toggleterm",
+				focus = true,
+				startinsert = true,
+				term = {
+					position = "bottom",
+					size = 15,
+				},
+				filetype = {
+					python = "cd $dir && python3 -u",
+					javascript = "cd $dir && node",
+					typescript = "cd $dir && tsc $fileName && node $fileNameWithoutExt.js",
+					rust = "cd $dir && rustc $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+					go = "cd $dir && go run",
+					java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
+					c = "cd $dir && gcc $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+					cpp = "cd $dir && g++ $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+					lua = "cd $dir && lua",
+					sh = "cd $dir && bash",
+					vue = "cd $dir && npm run dev $end",
+				},
+			})
+		end,
+	},
 }
