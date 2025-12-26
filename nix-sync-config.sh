e961361776f7e20b97058d9f412ba0ef15da8d51
EunoiaCody <eunoiacody@gmail.com>
2025-12-26 21:46:48 +0800

    更新配置文件 - 2025-12-26 21:46:48\n\n自动同步 Nix 环境下的变更

--- nix-sync-config.sh
+++ nix-sync-config.sh
@@ -56,8 +56,7 @@ if test -x "$DARWIN_REBUILD"
     if sudo env http_proxy=http://127.0.0.1:20122 \
             https_proxy=http://127.0.0.1:20122 \
             all_proxy=socks5://127.0.0.1:20122 \
-            $DARWIN_REBUILD switch --flake . \
-            --option substituters "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store https://cache.nixos.org"
+            $DARWIN_REBUILD switch --flake . 
             
         echo (set_color green)"✨ 系统与用户配置已同步完成！"(set_color normal)
     else
