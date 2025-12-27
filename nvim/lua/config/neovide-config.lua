21427840e09e703479813f9c2ae08376a936e628
EunoiaCody <eunoiacody@gmail.com>
2025-12-27 22:33:47 +0800

    更新配置文件 - 2025-12-27 22:33:47
    自动同步 ~/.config/ 中的配置文件到 dotfiles 仓库

--- nvim/lua/config/neovide-config.lua
+++ nvim/lua/config/neovide-config.lua
@@ -10,6 +10,6 @@ if vim.g.neovide then
 		":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>"
 	)
 	vim.g.neovide_window_blurred = true
-	vim.g.neovide_opacity = 0.9 -- 背景透明
+	vim.g.neovide_opacity = 0.6 -- 背景透明
 	vim.g.neovide_normal_opacity = 0.6
 end
