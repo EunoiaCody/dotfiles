3fdbdaefc32e864791badd2cfaf4e0075fce0e54
EunoiaCody <eunoiacody@gmail.com>
2025-12-27 08:28:10 +0800

    更新配置文件 - 2025-12-27 08:28:10\n\n自动同步 Nix 环境下的变更

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
     if test -e /etc/static/fish/config.fish
         source /etc/static/fish/config.fish
     end
+    set -gx EDITOR nvim
 end
 
 # Created by `pipx` on 2025-12-24 08:30:05
