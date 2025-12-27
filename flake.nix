992d2c88ff303073a4a0d46e9f0ba3b3a36633d1
EunoiaCody <eunoiacody@gmail.com>
2025-12-27 05:59:28 +0000

    更新配置文件 - 2025-12-27 05:59:28\n\n自动同步 Nix 环境下的变更

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    inputs.nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
+    nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
   };
 
   outputs = { self, nixpkgs, nix-darwin, home-manager, nixos-cosmic, ... }@inputs:
