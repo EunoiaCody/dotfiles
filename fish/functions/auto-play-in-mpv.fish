083b97f431cbc45c1289efec9cb107dd86cea109
EunoiaCody <eunoiacody@gmail.com>
2026-02-10 17:51:00 +0800

    更新配置文件 - 2026-02-10 17:51:00
    自动同步 ~/.config/ 中的配置文件到 dotfiles 仓库

--- fish/functions/auto-play-in-mpv.fish
+++ fish/functions/auto-play-in-mpv.fish
@@ -1,5 +1,5 @@
 function auto-play-in-mpv
-    set url (pbpaste)
+    set url (wl-paste)
     mpv --ytdl-raw-options=cookies-from-browser=chrome \
         --ytdl-format="bestvideo[height<=2160]+bestaudio/best" \
         $url
